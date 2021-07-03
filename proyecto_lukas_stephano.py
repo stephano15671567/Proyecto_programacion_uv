bd963e0da8ba3f449580ea9b01c86d793e88c1e3
LukasScheel <86583149+LukasScheel@users.noreply.github.com>
2021-07-03 00:36:15 -0400

    Avance
    Se avanza en el código poniendo condicionales.

--- proyecto_lukas_stephano.py
+++ proyecto_lukas_stephano.py
@@ -1,9 +1,7 @@
 print('Bienvenido a tu informativo de los datos actualizados sobre el covid-19 en Chile','\n','Aquí se mostrara información sobre el proceso de vacunación en Chile')
 print('Elija una de las siguientes tres opciones')
 print(' 1-Informacion por region sobre la vacunación','\n','2-Información general de la vacunación en Chile')
-
 y = input()
-
 while not y.isnumeric():
     print('Debe ingresar un valor valido porfavor')
     y = input()
@@ -14,6 +12,13 @@ if y=='1':
     print('Estas son las regiones de chile:','\n','15-Arica y parinacota','\n','1-Tarapaca','\n','2-Antofagasta','\n','3-Atacama','\n','4-Coquimbo','\n','5-Valparaiso','\n','13-Metropolitana','\n',"6-Bernardo O'higgins",'\n','7-Maule','\n','8-Bío bío','\n','9-Araucania','\n','14-Los rios','\n','10-Los lagos','\n','11-Aysen','\n','12-Magallanes')
     print('Seleccione una region para ver la comparación entre la cantidad de personas que se vacunan con la primera dosis y la segunda dosis')
     x = input()
+    while not x.isnumeric():
+        print('Debe ingresar un valor valido porfavor')
+        x = input()
+    while int(x) <=0 or int(x) >15:
+        print('Ingrese un valor valido porfavor')
+        x = input()
+    archivo = open('vacunacion_region.csv','r')
     def regiones(x):
         switcher={
             15:'arica y parinacota',
@@ -32,6 +37,11 @@ if y=='1':
             11:'Aysen',
             12:'Magallanes'}
         return switcher.get(x,"region invalida")
+    
+    
+    
+    
+
     
     
 
